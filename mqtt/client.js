f0996e8628e20c2449d022e3c4493f6cc846c45c
Chris Klein <chrisklein@nomabi.com>
2013-01-20 18:38:31 -0700

    switching out mqtt for now

--- mqtt/client.js
+++ mqtt/client.js
@@ -5,7 +5,7 @@ var mqtt = require('mqttjs')
 // Module
 exports.init = function(){
 
-  mqtt.createClient(1883, "localhost", function(err, client) {
+  mqtt.createClient(1883, "broker.mqttdashboard.com", function(err, client) {
 
     if (err) {
       console.log("ERROR:" + err);
